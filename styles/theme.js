d196ede9fa760ae343b7cfcb0aa3b8fdb838d88d
Christian Ranebäck <christian.raneback@epiceros.com>
2021-05-02 22:01:58 +0200

    Changed so initialColorMode is dark.

--- styles/theme.js
+++ styles/theme.js
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react"
 
 const extensions = {
     config: {
-        initialColorMode: "light",
+        initialColorMode: "dark",
         useSystemColorMode: false,
     },
     styles: {
