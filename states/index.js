131e56a0aedcfdc2b62b751a802f42111b17ec5d
Christian Ranebäck <christian.raneback@epiceros.com>
2021-05-03 22:20:10 +0200

    Make state persistant

--- states/index.js
+++ states/index.js
@@ -1,26 +1,28 @@
 import {atom} from "recoil";
+import { recoilPersist } from 'recoil-persist'
 
-export const viewState = atom({
-    key: 'viewState',
-    default: 'players',
-});
+const { persistAtom } = recoilPersist();
 
 export const nrOfPlayersPerTeamState = atom({
     key: 'nrOfPlayersPerTeamState',
     default: 2,
+    effects_UNSTABLE: [persistAtom],
 });
 
 export const playersState = atom({
     key: 'playersState',
     default: [],
+    effects_UNSTABLE: [persistAtom],
 });
 
 export const benchState = atom({
     key: 'benchState',
     default: [],
+    effects_UNSTABLE: [persistAtom],
 });
 
 export const teamsState = atom({
     key: 'teamsState',
     default: [],
+    effects_UNSTABLE: [persistAtom],
 });
